eef993c4ef46ddc0d3d10a88a0453e7dd711bcff
alec <alecsquire@gmail.com>
2025-01-11 08:29:45 +0000

    added data table

---
+++ resources/js/Pages/Items/Index.tsx
@@ -0,0 +1,15 @@
+import { Payment, columns } from "./columns";
+import { DataTable } from "./DataTable";
+
+interface Props {
+    payments?: Payment[]; // Make payments optional
+}
+
+export default function Items({ payments = [] }: Props) {
+    // Provide default empty array
+    return (
+        <div className="container mx-auto py-10">
+            <DataTable columns={columns} data={payments} />
+        </div>
+    );
+}
